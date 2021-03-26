72a149f8b23b6f24c7073b9d149b37879f6a210f
Trey Davis <trey@jwdavis.tech>
2021-03-26 11:51:49 -0400

    Initial Commit

---
+++ QuickMaths/QuickMathsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickMathsApp.swift
+//  QuickMaths
+//
+//  Created by Trey Davis on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct QuickMathsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
